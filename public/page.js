cdaaab9dc720ae7cb5e360c5a97f9f7f84a00bee
yuyu-10 <104772989+yuyu-10@users.noreply.github.com>
2023-03-07 11:07:22 +0100

    probleme menu version mobile resolu

--- public/page.js
+++ public/page.js
@@ -1,37 +1,47 @@
+const back = document.getElementById('hamburger-menu');
+const enora = document.getElementById('eno');
+const menu = document.getElementById('menu');
+const cross = document.getElementById('cross');
+const street = document.getElementById('street');
+const imgCar = document.getElementById("img-car");
+const street2 = document.getElementById('street-2-mobile');
+const street3 = document.getElementById('street-3-mobile');
+const sectionPeace = document.getElementById('section-peace');
+const sectionPortrait = document.getElementById('section-portrait');
+const footer = document.getElementById('img-footer');
+
 const btnMenu = document.getElementById('btn-menu');
 btnMenu.addEventListener("click", openMenu);
 function openMenu() {
-    const back = document.getElementById('hamburger-menu');
-    const enora = document.getElementById('eno');
-    const menu = document.getElementById('menu');
-    const cross = document.getElementById('cross');
-    const street = document.getElementById('street');
-    const imgCar = document.getElementById("img-car");
     btnMenu.style.display = "none";
     back.style.display = "inline";
     enora.style.zIndex = -1;
     menu.style.display = "flex";
     cross.style.display = "inline";
     street.style.display = "none";
-    imgCar.style.visibility = "hidden"
+    imgCar.style.visibility = "hidden";
+    street2.style.display = 'none';
+    street3.style.display = 'none';
+    sectionPeace.style.display = 'none';
+    sectionPortrait.style.display = 'none';
+    footer.style.display = 'none';
 }
 
 const btnCloseMenu = document.getElementById('cross');
 btnCloseMenu.addEventListener("click", closeMenu);
 function closeMenu() {
-    const back = document.getElementById('hamburger-menu');
-    const enora = document.getElementById('eno');
-    const menu = document.getElementById('menu');
-    const cross = document.getElementById('cross');
-    const street = document.getElementById('street');
-    const imgCar = document.getElementById("img-car");
     btnMenu.style.display = "inline";
     back.style.display = "none";
     enora.style.zIndex = 0;
     menu.style.display = "none";
     cross.style.display = "none";
     street.style.display = "flex";
-    imgCar.style.visibility = "visible"
+    imgCar.style.visibility = "visible";
+    street2.style.display = 'inline-block';
+    street3.style.display = 'inline-block';
+    sectionPeace.style.display = 'inline-block';
+    sectionPortrait.style.display = 'inline-block';
+    footer.style.display = 'inline-block';
 }
 
 // const buttonStreet = document.getElementById('button-street');
